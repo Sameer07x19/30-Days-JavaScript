08a46674fe6cf8e248d6bd7331a359d52db46c4a
Sameer Sayyad <sameer07x19@gmail.com>
2024-07-15 06:21:44 +0530

    add File1 & File2

---
+++ Day1/index.js
@@ -0,0 +1,44 @@
+// Task 1
+var a = 5;
+console.log(a);
+
+// Task 2
+let b = "Hello";
+console.log(b);
+
+// Task 3
+const c = true;
+console.log(c);
+
+// Task 4
+let num = 2;
+let str = "String Example";
+let bool = false;
+let obj = {
+    name: "Sameer Sayyad",
+    age: 20
+};
+let arr = ["abc",123,true];
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof bool);
+// console.log(typeof obj);
+// console.log(typeof arr);
+
+console.table([typeof num,typeof str,typeof bool,typeof obj,typeof arr]) //💪
+
+// Task 5
+let test1 = 6;
+console.log("initial value: " + test1)
+test = 7;
+console.log("New value: " + test1)
+
+// Task 6
+const test2 = 8;
+console.log("initial value: " + test2)
+// cannot redeclare or assign a constant variable
+// test2 = 7; 
+// console.log("New value: " + test2)
+
+// Day 1 Completed 🌊
---
+++ Day2/index.js
@@ -0,0 +1,47 @@
+let a = 3
+let b = 2
+
+// Task 1-5
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+
+// Task 6
+// a += 5;
+// console.log(a);
+
+//Task 7
+// b -= 2
+// console.log(b);
+
+// Task 8
+// console.log(a > b);
+// console.log(a < b);
+
+// Task 9
+// console.log(a >= b);
+// console.log(a <= b);
+
+// Task 10
+let c = 10,d = "10"
+// console.log(c == d);
+// console.log(c === d);
+
+// Task 11
+let e = a > b && c == d
+// console.log(e);
+
+// Task 12
+let f = a > b || c === d
+// console.log(f);
+
+// Task 13
+// console.log(c !== d);
+
+// Task 14
+let g = -10
+// console.log(g >= 0 ? "Positive" : "Negative");
+
+// Day 2 completed 🌊
